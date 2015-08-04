e40f8c568ef185238b15b9b0aa4658c5c7d741b7
Xingchen Hong <hello@xc-h.net>
2015-08-03 22:05:18 -0500

    Minor changes.

--- package.js
+++ package.js
@@ -12,7 +12,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.use('invictus:extjs');
+  api.use('invictus:extjs@0.3.0');
   api.addFiles('ext-require.js', 'client');
 });
 
